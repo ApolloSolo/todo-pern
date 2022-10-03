0740011bfe9ff21b78e5251e35f06552b1f78304
AppSolo <appsolo.tech@gmail.com>
2022-10-03 18:48:52 -0400

    Added calc for days left to complete to-do

--- server/controllers/todoControllers.js
+++ server/controllers/todoControllers.js
@@ -1,4 +1,5 @@
 const db = require("../connection/db");
+const { daysLeft } = require("../utils/time_date");
 
 const getUserTodos = async (req, res) => {
   try {
@@ -26,6 +27,11 @@ const getOneTodo = async (req, res) => {
       [username, todo_id]
     );
     if (oneTodo.rows.length > 0) {
+      let timeLeft = daysLeft(
+        oneTodo.rows[0].due_date,
+        oneTodo.rows[0].start_date
+      );
+      oneTodo.rows[0].daysLeft = timeLeft;
       res.json(oneTodo.rows[0]);
     } else {
       throw new Error("No todo");
@@ -87,7 +93,7 @@ const deleteTodo = async (req, res) => {
     res.json({ error: error.message });
   }
 };
-1
+1;
 module.exports = {
   getUserTodos,
   getOneTodo,
